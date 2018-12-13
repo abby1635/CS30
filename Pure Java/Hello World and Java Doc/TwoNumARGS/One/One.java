a7b529a55339852d9d36b54a53f3b442c1591393
MercersKitchen <Mark.Mercer@epsb.ca>
2018-12-13 11:50:02 -0700

    Update

---
+++ Pure Java/Hello World and Java Doc/TwoNumARGS/One/One.java
@@ -0,0 +1,19 @@
+public class One
+{
+  public static void main (String[] args)
+  throws ArrayIndexOutOfBoundsException
+  {
+    try{ //Did someone enter the required arguments
+      int i=0;
+      while(args[i] == "") { //One Reason this will return false or error
+        // No argument entered and exception thrown
+        // Note: Argument entered so args[0] is NOT null, will run rest of code b/c WHILE
+      }
+    }
+    catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("You must enter arguments (two numbers) to start this program. \n" +
+                         "Please restart the program with arguments. Thank-you");
+      System.exit(0);
+    }
+  }
+}
---
+++ Pure Java/Hello World and Java Doc/TwoNumARGS/README.md
@@ -0,0 +1,15 @@
+# Adding Two Numbers and using ARGS[]
+A Project illustrating how to build a method anticipating errors
+
+**UNDER CONSTRUCTION**
+
+
+---
+
+# Ideas to Include
+
+List of Errors (Numbering refers to program name too)
+1. Args[]: no values entered, ArrayIndexOutOfBounds
+2.
+
+---
